744ba0be4eebad354c12469bafc30b7e1ae18ac4
heartylearner <71243424+heartylearner@users.noreply.github.com>
2022-12-03 08:38:22 +0800

    Add files via upload
    upload betti.py with batch-processing api and sample usage in shell script

--- yolov7/betti.py
+++ yolov7/betti.py
@@ -3,6 +3,7 @@ import time
 #from pycuda import driver
 import argparse
 import os
+from pathlib import Path
 import numpy as np
 from copy import deepcopy
 device_idx=0
@@ -18,48 +19,9 @@ split_size=1024
 from torch.multiprocessing import Process
 import multiprocessing as mp
 from multiprocessing import shared_memory
-#import skimage.measure
-images=[
-    [
-        [[1,0,0],[1,0,0],[1,0,0]],
-        [[1,0,0],[0,1,0],[1,0,0]],
-        [[1,0,0],[1,0,0],[1,0,0]],
-    ],
-    [
-        [[1,0,0],[1,0,0],[1,0,0],[1,0,0],[1,0,0]],
-        [[1,0,0],[1,0,0],[0,1,0],[1,0,0],[1,0,0]],
-        [[1,0,0],[0,1,0],[0,1,0],[0,1,0],[1,0,0]],
-        [[1,0,0],[1,0,0],[0,1,0],[1,0,0],[1,0,0]],
-        [[1,0,0],[1,0,0],[1,0,0],[1,0,0],[1,0,0]]
-    ]
-]
-# class gpuThread(threading.Thread):
-#     def __init__(self, gpuid,f,id,P,S,u0,v0,u1,v1,U0,V0,W,H,BC):
-#         threading.Thread.__init__(self)
-#         print("gpuid=",gpuid)
-#         self.f=f
-#         self.id=id
-#         self.P=P
-#         self.S=S
-#         self.u0=u0
-#         self.u1=u1
-#         self.v0=v0
-#         self.v1=v1
-#         self.U0,self.V0=U0,V0
-#         self.W,self.H=W,H
-#         self.ctx  = driver.Device(gpuid).make_context()
-#         self.device = self.ctx.get_device()
-#         self.BC=BC
-#     def run(self):
-#         self.t=time.time()
-#         self.f(self.id,self.P,self.S,self.u0,self.v0,self.u1,self.v1,self.U0,self.V0,self.W,self.H,self.BC)
-        
-        # Profit!
+import glob
+images=[]
 
-    # def join(self):
-    #     self.ctx.detach()
-    #     threading.Thread.join(self)
-    #     print(f"thread u0={self.u0} v0={self.v0} u1={self.u1} v1={self.v1} run {time.time()-self.t} s")
 def device_assign():
     global device_idx
     device_idx=(device_idx+1)%devices_count
@@ -171,19 +133,7 @@ def cal(id,P,S,u0,v0,u1,v1,U0,V0,W,H,BC,num_workers=1):
     #print(f"cal id={id} u0={u0} v0={v0} u1={u1} v1={v1} BC={BC[0]}")
     if(u1-u0>=split_size and num_workers>1):
         um=(u1+u0)//2
-        #BC1=deepcopy(BC)
-        # sm1=shared_memory.SharedMemory(create=True, size=2)
-        # sm2=shared_memory.SharedMemory(create=True, size=2)
-        # BC1=sm1.buf
-        # BC2=sm2.buf
-        #BC1[0]=-1
-        # BC1[0]=0
-        # BC2[0]=0
-        #BC1=mp.Array('i', [0])
         BC2=mp.Array('i', [0])
-        #t0=gpuThread(device_assign(),cal,id,P,S,u0,v0,um,v1,U0,V0,W,H,BC)
-        #t1=gpuThread(device_assign(),cal,id,P,S,um+1,v0,u1,v1,U0,V0,W,H,BC1)
-        #BCshared_memory.SharedMemory(create=True, size=1)
         t0=Process(target=cal,args=(id,P,S,u0,v0,um,v1,U0,V0,W,H,BC,num_workers//2))
         t1=Process(target=cal,args=(id,P,S,um+1,v0,u1,v1,U0,V0,W,H,BC2,num_workers//2))
         t0.start()
@@ -191,30 +141,11 @@ def cal(id,P,S,u0,v0,u1,v1,U0,V0,W,H,BC,num_workers=1):
         t0.join()
         t1.join()
         BC[0]+=BC2[0]
-        #BC1.close()
-        #BC2.close()
-        # sm1.close()
-        # sm2.close()
-        # sm1.unlink()
-        # sm2.unlink()
-        t=time.time()
-        #print(f"before mergeu u0={um} v0={v0} u1={um+1} v1={v1} BC={BC[0]}")
-        
+        t=time.time()        
         mergeu(id,P,S,um,v0,um+1,v1,U0,V0,W,H,BC)
-        #print(f"after mergeu u0={um} v0={v0} u1={um+1} v1={v1} BC={BC[0]} {time.time()-t}s")
     elif(v1-v0>=split_size and num_workers>1):
         vm=(v1+v0)//2
-        #BC1=deepcopy(BC)
-        #sm1=shared_memory.SharedMemory(create=True, size=2)
-        #sm2=shared_memory.SharedMemory(create=True, size=2)
-        # BC1=sm1.buf
-        # BC2=sm2.buf
-        # BC1[0]=0
-        # BC2[0]=0
-        #BC1=mp.Array('i', [0])
         BC2=mp.Array('i', [0])
-        #t0=gpuThread(device_assign(),cal,id,P,S,u0,v0,u1,vm,U0,V0,W,H,BC)
-        #t1=gpuThread(device_assign(),cal,id,P,S,u0,vm+1,u1,v1,U0,V0,W,H,BC1)
         t0=Process(target=cal,args=(id,P,S,u0,v0,u1,vm,U0,V0,W,H,BC,num_workers//2))
         t1=Process(target=cal,args=(id,P,S,u0,vm+1,u1,v1,U0,V0,W,H,BC2,num_workers//2))
         t0.start()
@@ -222,17 +153,7 @@ def cal(id,P,S,u0,v0,u1,v1,U0,V0,W,H,BC,num_workers=1):
         t0.join()
         t1.join()
         BC[0]+=BC2[0]
-        #print(f"before mergev u0={u0} v0={vm} u1={u1} v1={vm+1} BC={BC[0]}")
-        #BC1.close()
-        #BC2.close()
-        # sm1.close()
-        # sm2.close()
-        # sm1.unlink()
-        # sm2.unlink()
-        t=time.time()
-        #BC2.unlink()
         mergev(id,P,S,u0,vm,u1,vm+1,U0,V0,W,H,BC)
-        #print(f"after mergev u0={u0} v0={vm} u1={u1} v1={vm+1} BC={BC[0]} {time.time()-t}s")
     else:
         for u in range(u0,u1):
             for v in range(v0,v1):
@@ -256,38 +177,20 @@ def cal(id,P,S,u0,v0,u1,v1,U0,V0,W,H,BC,num_workers=1):
                     d1011=dist(images[id][v][u+1],images[id][v+1][u+1])
                     if(d0010<=thres and d1011<thres):BC[0]-=1
                     if(d0111<=thres and d0001<=thres):BC[0]-=1
-                # if((u0,v0,u1,v1)==(0,0,8,8)):
-                #     print(u,v,d0010,d0001,d0110,d0011,d0111,d1011,BC)
             if(d0111<=thres):
                 union(P,S,u,v1,u+1,v1,U0,V0,W,H,BC)
-                # if((u0,v0,u1,v1)==(0,0,8,8)):
-                #     print(u,v1,d0010,d0001,d0110,d0011,d0111,d1011,BC)
         for v in range(v0,v1):
             d0001=dist(images[id][v][u1],images[id][v+1][u1])
             if(d0001<=thres):union(P,S,u1,v,u1,v+1,U0,V0,W,H,BC)
-            # if((u0,v0,u1,v1)==(0,0,8,8)):
-            #     print(u1,v,BC)
-        # check_id=check(P,S,u0,v0,u1,v1,U0,V0,W,H)
-        # if(check_id!=-1):
-        #     print(u0,v0,u1,v1,"fail at",check_id%W,check_id//W)
-
-    #print(f"cal id={id} u0={u0} v0={v0} u1={u1} v1={v1} BC={BC[0]}")
-        
-
 def betti(id,pool,pool_idx,u0,v0,u1,v1,num_workers=1):
-    #BC=shared_memory.SharedMemory(create=True, size=1)
     BC=mp.Array('i',[0],lock=False)
     P=-1*np.ones(((v1-v0+1)*(u1-u0+1),))
     P=mp.Array('f',P.tolist(),lock=False)
     S=np.ones(((v1-v0+1)*(u1-u0+1),))
     S=mp.Array('f',S.tolist(),lock=False)
     cal(id,P,S,u0,v0,u1,v1,u0,v0,u1-u0+1,v1-v0+1,BC,num_workers)
-    #print(P)
-    #print(S)
-    #print("betti return ",BC[0])
     pool[pool_idx]=BC[0]
 def betti_pool(id,w,h,num_workers):
-    #print("in betti",images[id][0][0])
     Nu=len(images[id][0])//w
     Nh=len(images[id])//h
     pool=mp.Array('i',[0 for i in range(Nu*Nh)],lock=False)
@@ -334,12 +237,12 @@ def draw_box(img,u0,v0,w,h):
         img[v0][u]=img[v0+1][u]=img[min(v0+h-1,len(img)-1)][u]=img[min(v0+h,len(img)-1)][u]=[255,0,0]
     for v in range(v0,min(v0+h,len(img))):
         img[v][u0]=img[v][u0+1]=img[v][min(u0+w-1,len(img[0])-1)]=img[v][min(u0+w,len(img[0])-1)]=[255,0,0]
-def draw_boxes(img,pool,thres):
+def draw_boxes(img,pool,betti_thres):
     W=len(img[0])//pool.shape[1]
     H=len(img)//pool.shape[0]
     for i in range(pool.shape[0]):
         for j in range(pool.shape[1]):
-            if(int(pool[i,j])>thres):
+            if(int(pool[i,j])>betti_thres):
                 r=img[H*i:H*i+H,W*j:W*j+W,0]
                 g=img[H*i:H*i+H,W*j:W*j+W,1]
                 b=img[H*i:H*i+H,W*j:W*j+W,2]
@@ -356,80 +259,59 @@ def draw_boxes(img,pool,thres):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--source', type=str, default='inference/images', help='source')  # file/folder, 0 for webcam
-    parser.add_argument('--img-size', type=int, default=640, help='inference size (pixels)')
-    parser.add_argument('--dist_thres', type=float, default=10, help='object confidence threshold')
-    parser.add_argument('--target_size', type=int, default=80, help='object confidence threshold')
-    parser.add_argument('--conv_stride', type=int, default=40, help='object confidence threshold')
-    parser.add_argument('--box_thres', type=float, default=0, help='object confidence threshold')
-    
-
+    parser.add_argument('--img_size', type=int, default=640, help='inference size (pixels)')
+    parser.add_argument('--dist_thres', type=float, default=50, help='distance threshold for connection')
+    parser.add_argument('--target_upper_size', type=int, default=80, help='target object typical biggest size')
+    parser.add_argument('--target_lower_size', type=int, default=20, help='target object typical smallest size')
+    #parser.add_argument('--conv_stride', type=int, default=40, help='object confidence threshold')
+    parser.add_argument('--betti_thres', type=int, default=0, help='lower bound of betti number for consideration of importance')
+    parser.add_argument('--num_workers', type=int, default=64, help='number of parallel process')
+    parser.add_argument('--save_dir', type=str, default='./', help='lower bound of betti number for consideration of importance')
     args = parser.parse_args()
     print(args)
+    p = str(Path(args.source).absolute())  # os-agnostic absolute path
+    if '*' in p:
+        files = sorted(glob.glob(p, recursive=True))  # glob
+    elif os.path.isdir(p):
+        files = sorted(glob.glob(os.path.join(p, '*.*')))  # dir
+    elif os.path.isfile(p):
+        files = [p]  # files
+    else:
+        raise Exception(f'ERROR: {p} does not exist')
+    img_formats=['jpg','png']
+    image_file = [x for x in files if x.split('.')[-1].lower() in img_formats]
+    images=[cv2.imread(img) for img in image_file]
     thres=args.dist_thres
-    image=cv2.imread(args.source)
-    img0=deepcopy(image)
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    image = cv2.resize(image, (640, 640), interpolation=cv2.INTER_LINEAR)
-
-
-    image=cv2.medianBlur(image,15)
-    image1=cv2.GaussianBlur(image,(5,5),0)
-    image=(4*np.array(image)-3*np.array(image1))
-    # cv2.imwrite("./g.jpg",image)
+    #image=cv2.imread(args.source)
+    for i in range(len(images)):
+        image=images[i]
+        img0=deepcopy(image)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        image = cv2.resize(image, (640, 640), interpolation=cv2.INTER_LINEAR)
+        image=cv2.medianBlur(image,15)
+        image1=cv2.GaussianBlur(image,(5,5),0)
+        image=(4*np.array(image)-3*np.array(image1))
+        image=image.reshape((args.img_size,args.img_size,1))
+        images[i]=image
+        poolU=betti_pool(i,args.target_upper_size,args.target_upper_size,args.num_workers)
+        poolL=betti_pool(i,args.target_lower_size,args.target_lower_size,args.num_workers)
+        print("poolU\n",poolU)
+        print("\npoolL\n",poolL)
+        #print(pool20[28:32,28:32])
+        M,N=poolU.shape
+        m,n=poolL.shape
+        poolL_U = np.transpose(poolL.reshape(M, m//M, N, n//N),axes=(0,2,1,3)).reshape(args.img_size//args.target_upper_size,args.img_size//args.target_upper_size,(m*n)//(M*N))
+        poolL_U=np.sum(poolL_U,axis=-1)
+        print("\npoolL_U\n",poolL_U)
+        print("\npoolU-poolL_U\n",poolU-poolL_U)
+        #img_pool=img0.tolist()
+        img_pool=deepcopy(img0)
+        print(img_pool.shape)
+        draw_boxes(img_pool,poolU-poolL_U,args.betti_thres)
+        print("save in",args.save_dir+'/'+image_file[i].split('/')[-1])
+        cv2.imwrite(args.save_dir+'/'+image_file[i].split('/')[-1],np.array(img_pool))
+        #exit()
+    f=open(args.save_dir+"/args.txt",'w')
+    f.write(str(args))
+    f.close()
     
-    #cv2.imwrite("./m_rg.jpg",image)
-    #image=cv2.equalizeHist(image)
-    #cv2.imwrite("./e.jpg",image)
-
-    image=image.reshape((640,640,1))
-    #thres=10
-    images=[image]+images
-    pool80=betti_pool(0,args.target_size,args.target_size,64)
-    pool20=betti_pool(0,args.target_size//4,args.target_size//4,64)
-    print("pool80\n",pool80)
-    print("\npool20\n",pool20)
-    #print(pool20[28:32,28:32])
-    m, n = pool20.shape
-    pool20_80 = np.transpose(pool20.reshape(m//4, 4, n//4, 4),axes=(0,2,1,3)).reshape(640//args.target_size,640//args.target_size,16)
-    pool20_80=np.sum(pool20_80,axis=-1)
-    print("\npool20_80\n",pool20_80)
-    print("\npool80-pool20_80\n",pool80-pool20_80)
-    #img_pool=img0.tolist()
-    img_pool=deepcopy(img0)
-    print(img_pool.shape)
-    draw_boxes(img_pool,pool80-pool20_80,args.box_thres)
-    cv2.imwrite(args.source[:-4]+f"_dist_{args.dist_thres}_betti_{args.box_thres}_pool.jpg",np.array(img_pool))
-    #cv2.imwrite("./m_rg_bbox.jpg",np.array(img0))
-    exit()
-    conv80=betti_conv(0,args.target_size,args.target_size,args.conv_stride,64)
-    print("\nconv80\n",conv80.shape,"\n",conv80)
-    m,n=conv80.shape
-    conv20=betti_conv(0,args.target_size//4,args.target_size//4,args.conv_stride//4,64)[:m*4,:n*4]
-    print("\nconv20\n",conv20.shape,"\n",conv20)    
-    m,n=conv20.shape
-    conv20_80= np.transpose(conv20.reshape(m//4, 4, n//4, 4),axes=(0,2,1,3)).reshape((640-args.target_size)//args.conv_stride+1,(640-args.target_size)//args.conv_stride+1,16)
-    conv20_80=np.sum(conv20_80,axis=-1)
-    print("\nconv20_80\n",conv20_80.shape,"\n",conv20_80)
-    #exit()
-    print("\nconv80-conv20_80\n",conv80-conv20_80)
-    img_conv=img0.tolist()
-    draw_boxes(img_conv,conv80-conv20_80,args.box_thres)
-    cv2.imwrite(args.source[:-4]+f"_dist_{args.dist_thres}_betti_{args.box_thres}_conv.jpg",np.array(img_conv))
-    
-#check_requirements(exclude=('pycocotools', 'thop'))
-
-# test_img_size=640
-# split_size=32
-# img=[[[1] for i in range(test_img_size)]for i in range(test_img_size)]
-# for i in range(test_img_size):
-#     for j in range(test_img_size):
-#         if(i%3==1 and j%3==1):
-#             img[i][j]=[0]
-# images=[img]+images
-# t=time.time()
-# id=0
-# print(betti(id,0,0,len(images[id][0])-1,len(images[id])-1,128))
-# betti_pool(0,64)
-# print(betti(id,0,0,320,320,0))
-# print(time.time()-t,"s")
-# print(np.squeeze(np.array(img)))
---
+++ yolov7/betti.sh
@@ -0,0 +1,1 @@
+python betti.py --source "testing_data/" --img_size 640 --dist_thres 50 --target_upper_size 80 --target_lower_size 20 --betti_thres -100 --num_workers 64 --save_dir "0"
